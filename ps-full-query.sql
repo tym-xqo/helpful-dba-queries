1fe92e971ea0934b84dc5aea67ee8c6679ea6f36
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2020-01-13 11:53:14 -0600

    add ps query with full query in result

---
+++ ps-full-query.sql
@@ -0,0 +1,13 @@
+select pid
+     , age(now(), xact_start) as xact_age
+     , age(now(), query_start) as query_age
+     , usename
+     , application_name
+     , query
+     , state
+  from pg_stat_activity
+  -- where application_name='psql'
+ where state != 'idle'
+   and backend_type = 'client backend'
+   and pid != pg_backend_pid()
+ order by query_start;
