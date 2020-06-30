7afc5cb854b8aa35cd6ec019c577b303828c14d1
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2020-06-30 11:11:43 -0500

    Show blocking query age in lock-queue.sql

--- lock-queue.sql
+++ lock-queue.sql
@@ -8,11 +8,12 @@ select b.blocked_pid
      , b.blocking_pid
      , left(a.query, 40) as blocked_query
      , left(c.query, 40) as blocking_query
-     , c.query_start as blocking_start
+     , age(now(), c.xact_start) as blocking_age
      , c.state as blocking_state
   from b
   join pg_stat_activity a
     on b.blocked_pid = a.pid
   join pg_stat_activity c
     on b.blocking_pid = c.pid 
+    -- where c.state = 'idle in transaction'
  order by c.query_start;
