7cbc59cd5b6554789d076f942d9d537a379510a4
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2020-07-21 09:39:06 -0500

    Add total to conns.sql

--- conns.sql
+++ conns.sql
@@ -4,4 +4,10 @@ select usename
      , max(age(now(), query_start)) max_query_age
   from pg_stat_activity
  group by usename, state
- order by usename, state;
+ union all
+ select 'total'
+      , 'total'
+      , count(*)
+      , max(age(now(), query_start))
+  from pg_stat_activity
+ order by state, usename nulls first;
